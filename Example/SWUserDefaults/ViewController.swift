3c549c82f5d0e46c971de3d2d41775e17e197c07
cellgit <liuhongli2120@gmail.com>
2022-12-16 14:58:34 +0800

    [t8]添加示例

--- Example/SWUserDefaults/ViewController.swift
+++ Example/SWUserDefaults/ViewController.swift
@@ -15,7 +15,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        SWUserDefaults.save(3, "key")
+        // save Int value to UserDefaults
+        SWUserDefaults.save(3, "key1")
+        // get Int value from UserDefaults
+        let value = SWUserDefaults.get("key1", Int.self)
+        // delete from UserDefaults
+        SWUserDefaults.delete("key1")
+
+
+        // save dict value to UserDefaults
+        let dict: [String : Any] = ["name": "Lily", "age": 18]
+        SWUserDefaults.save(dict, "key2")
+        // get dict value from UserDefaults
+        let dictValue = SWUserDefaults.get("key2", [String : Any].self)
+        // delete from UserDefaults
+        SWUserDefaults.delete("key2")
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -20,6 +20,25 @@ it, simply add the following line to your Podfile:
 pod 'SWUserDefaults'
 ```
 
+##Usage
+```
+// save Int value to UserDefaults
+SWUserDefaults.save(3, "key1")
+// get Int value from UserDefaults
+let value = SWUserDefaults.get("key1", Int.self)
+// delete from UserDefaults
+SWUserDefaults.delete("key1")
+
+
+// save dict value to UserDefaults
+let dict: [String : Any] = ["name": "Lily", "age": 18]
+SWUserDefaults.save(dict, "key2")
+// get dict value from UserDefaults
+let dictValue = SWUserDefaults.get("key2", [String : Any].self)
+// delete from UserDefaults
+SWUserDefaults.delete("key2")
+```
+
 ## Author
 
 liuhongli, liuhongli2120@gmail.com
